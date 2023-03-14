9e77a9dae31d5088d902e0b470d4d174ccd2992e
bibhuti9 <bibhutiswain990@gmail.com>
2023-03-14 23:43:04 +0530

    react native new editor

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+const config = {
+    //  editorconfig
+    editorconfig: true,
+
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+};
+
+module.exports = config;
